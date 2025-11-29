60d262f148a3f53d8fcc67bb5ef818791537194e
ayaki-coder <sj.a.yakimovich@gmail.com>
2025-11-29 22:05:02 +0300

    feat: add structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ArtistModule } from './artist/artist.module';
+import { FavoritesModule } from './favorites/favorites.module';
+import { TrackModule } from './track/track.module';
+import { AlbumModule } from './album/album.module';
+import { UserModule } from './user/user.module';
+import { InMemoryDatabaseModule } from './in-memory-database/in-memory-database.module';
+import { InMemoryService } from './database/in-memory-/in-memory-.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ArtistModule,
+    AlbumModule,
+    TrackModule,
+    FavoritesModule,
+    UserModule,
+    InMemoryDatabaseModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, InMemoryService],
 })
 export class AppModule {}
---
+++ src/in-memory-database/in-memory-database.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class InMemoryDatabaseService {
+  // public readonly users: User[] = [];
+  // public readonly artists: Artist[] = [];
+  // public readonly albums: Album[] = [];
+  // public readonly tracks: Track[] = [];
+  // public readonly favorites: Favorite[] = [];
+}
