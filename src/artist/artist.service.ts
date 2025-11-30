8bbbe8926a61ca9200df894f106caf1670e84197
ayaki-coder <sj.a.yakimovich@gmail.com>
2025-11-30 03:53:18 +0300

    feat: add artists

--- src/artist/artist.service.ts
+++ src/artist/artist.service.ts
@@ -1,29 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateArtistDto } from './dto/create-artist.dto';
 import { UpdateArtistDto } from './dto/update-artist.dto';
 import { InMemoryDatabaseService } from '../in-memory-database/in-memory-database.service';
+import { Artist } from './entities/artist.entity';
 
 @Injectable()
 export class ArtistService {
   constructor(private readonly db: InMemoryDatabaseService) {}
 
-  create(createArtistDto: CreateArtistDto) {
-    return 'This action adds a new artist';
+  create(createArtistDto: CreateArtistDto): Artist {
+    return this.db.createArtist(createArtistDto);
   }
 
-  findAll() {
-    return `This action returns all artist`;
+  findAll(): Artist[] {
+    return this.db.getAllArtists();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} artist`;
+  findOne(id: string): Artist {
+    const artist = this.db.getArtistById(id);
+    if (!artist) {
+      throw new NotFoundException();
+    }
+
+    return artist;
   }
 
-  update(id: number, updateArtistDto: UpdateArtistDto) {
-    return `This action updates a #${id} artist`;
+  update(id: string, updateArtistDto: UpdateArtistDto): Artist {
+    const artist = this.db.getArtistById(id);
+    if (!artist) {
+      throw new NotFoundException();
+    }
+
+    return this.db.updateArtist(id, updateArtistDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} artist`;
+  remove(id: string): Artist {
+    const deletedArtist = this.db.deleteArtist(id);
+    if (!deletedArtist) {
+      throw new NotFoundException();
+    }
+
+    const relatedTracks = this.db
+      .getAllTracks()
+      .filter((track) => track.artistId === deletedArtist.id);
+
+    relatedTracks.forEach((track) =>
+      this.db.updateTrack(track.id, { ...track, artistId: null }),
+    );
+
+    const relatedAlbums = this.db
+      .getAllAlbums()
+      .filter((album) => album.artistId === deletedArtist.id);
+
+    relatedAlbums.forEach((album) =>
+      this.db.updateAlbum(album.id, { ...album, artistId: null }),
+    );
+
+    return deletedArtist;
   }
 }
