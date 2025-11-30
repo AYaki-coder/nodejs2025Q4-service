1430038b6b2692d28f3d8b773fa795b2a15fbafb
ayaki-coder <sj.a.yakimovich@gmail.com>
2025-11-30 04:07:50 +0300

    feat: add albums

--- src/album/album.service.ts
+++ src/album/album.service.ts
@@ -1,29 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateAlbumDto } from './dto/create-album.dto';
 import { UpdateAlbumDto } from './dto/update-album.dto';
 import { InMemoryDatabaseService } from '../in-memory-database/in-memory-database.service';
+import { Album } from './entities/album.entity';
 
 @Injectable()
 export class AlbumService {
   constructor(private readonly db: InMemoryDatabaseService) {}
 
-  create(createAlbumDto: CreateAlbumDto) {
-    return 'This action adds a new album';
+  create(createAlbumDto: CreateAlbumDto): Album {
+    return this.db.createAlbum(createAlbumDto);
   }
 
-  findAll() {
-    return `This action returns all album`;
+  findAll(): Album[] {
+    return this.db.getAllAlbums();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} album`;
+  findOne(id: string): Album {
+    const album = this.db.getAlbumById(id);
+    if (!album) {
+      throw new NotFoundException();
+    }
+
+    return album;
   }
 
-  update(id: number, updateAlbumDto: UpdateAlbumDto) {
-    return `This action updates a #${id} album`;
+  update(id: string, updateAlbumDto: UpdateAlbumDto): Album {
+    const album = this.db.getAlbumById(id);
+    if (!album) {
+      throw new NotFoundException();
+    }
+
+    return this.db.updateAlbum(id, updateAlbumDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} album`;
+  remove(id: string): Album {
+    const deletedAlbum = this.db.deleteAlbum(id);
+    if (!deletedAlbum) {
+      throw new NotFoundException();
+    }
+
+    const relatedTracks = this.db
+      .getAllTracks()
+      .filter((track) => track.albumId === deletedAlbum.id);
+
+    relatedTracks.forEach((track) =>
+      this.db.updateTrack(track.id, { ...track, albumId: null }),
+    );
+
+    return deletedAlbum;
   }
 }
--- src/album/dto/create-album.dto.ts
+++ src/album/dto/create-album.dto.ts
@@ -1,5 +1,14 @@
+import { IsInt, IsNotEmpty, IsString, ValidateIf } from 'class-validator';
+
 export class CreateAlbumDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+  @IsInt()
+  @IsNotEmpty()
   year: number;
+  @ValidateIf((album) => album.artistId !== null)
+  @IsString()
+  @IsNotEmpty()
   artistId: string | null;
 }
